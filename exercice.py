b3b9fe78798cf8a7b885d239acacf14e81fb2249
fabricerenard12 <fabrice.renard12@outlook.com>
2022-10-17 23:07:33 -0400

    Exercices complétés

--- exercice.py
+++ exercice.py
@@ -4,16 +4,38 @@
 import random
 
 def get_first_part_of_name(name):
-	return ""
+	firstName = name.split('-')[0]
+	
+	return 'Bonjour, ' + firstName.lower().capitalize()
 
 def get_random_sentence(animals, adjectives, fruits):
-	return ""
+	return f"Aujourd’hui, j’ai vu un {animals[random.randrange(0, len(animals))]} s’emparer d’un panier {adjectives[random.randrange(0, len(adjectives))]} plein de {fruits[random.randrange(0, len(fruits))]}."
 
 def encrypt(text, shift):
-	return ""
+	alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	res = ''
+
+	for i in text:
+		if i.upper() in alphabet:
+			res += alphabet[(alphabet.index(i.upper()) + shift) % len(alphabet)].upper()
+		else:
+			res += i
+
+
+	return res
 
 def decrypt(encrypted_text, shift):
-	return ""
+	alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	res = ''
+
+	for i in encrypted_text:
+		if i.upper() in alphabet:
+			res += alphabet[(alphabet.index(i.upper()) - shift) % len(alphabet)].upper()
+		else:
+			res += i
+
+
+	return res
 
 
 if __name__ == "__main__":
